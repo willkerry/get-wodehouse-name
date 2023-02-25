84bf018b1defa4b8573622b4271438523002969a
Will Kerry <will.kerry@hbsuk.co.uk>
2023-02-25 09:11:39 +0000

    Reun Prettier

--- src/getRandomName.ts
+++ src/getRandomName.ts
@@ -1,8 +1,8 @@
 import { NAMES } from "./names";
-import type {Name} from "./types/Name";
+import type { Name } from "./types/Name";
 
 const getRandomName = () => {
-    return NAMES[Math.floor(Math.random() * NAMES.length)] as Name;
+  return NAMES[Math.floor(Math.random() * NAMES.length)] as Name;
 };
 
 export { getRandomName };
